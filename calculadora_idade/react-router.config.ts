1d04473ba9f604ca4a30eb797345cf8eaa69b13f
beatrizrosa <rosabeatriz438@yahoo.com.br>
2025-09-13 09:18:36 -0300

    Ajuste4

---
+++ calculadora_idade/react-router.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "@react-router/dev/config";
+// import { vercelPreset } from '@react-router/dev/presets/vercel';
+
+export default {
+  // Config options...
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
+  //  presets: [vercelPreset()],
+} satisfies Config;
